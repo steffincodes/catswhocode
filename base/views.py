699d0ed19e4d1a1fa6d133854c11af379a27d928
Steffin <steffin.codes@gmail.com>
2022-05-27 18:47:12 +0530

    🔨 Rendered view from template with proper config

--- base/views.py
+++ base/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home Page')
+    # return HttpResponse('Home Page')
+    return render(request, 'home.html')
 
 def catRoom(request):
-    return HttpResponse('Cat Room')
+    # return HttpResponse('Cat Room')
+    return render(request, 'catRoom.html')
