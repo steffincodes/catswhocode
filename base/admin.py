fe199e6a05b911f1b75c8785e7d1ec9c5a2683ea
Steffin <steffin.codes@gmail.com>
2022-05-30 10:32:04 +0530

    🗃️ Added Topic and Meow models

--- base/admin.py
+++ base/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-# from .models import Topic,CatRoom,Meow
-from .models import CatRoom
+from .models import Topic,CatRoom,Meow
 
+admin.site.register(Topic)
 admin.site.register(CatRoom)
+admin.site.register(Meow)
--- base/models.py
+++ base/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-# class Topic(models.Model):
-#     name = models.CharField(max_length=200)
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
 
-#     def __str__(self):
-#         return str(self.name)
+    def __str__(self):
+        return str(self.name)
 
 
 class CatRoom(models.Model):
-    # topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(
         null=True, blank=True)  # can be blank in db & UI
@@ -22,12 +22,12 @@ class CatRoom(models.Model):
         return str(self.name)
 
 
-# class Meow(models.Model):
-#     cat = models.ForeignKey(User, on_delete=models.CASCADE)
-#     catRoom = models.ForeignKey(CatRoom, on_delete=models.CASCADE)
-#     body = models.TextField()
-#     updated = models.DateTimeField(auto_now=True)  # every saved change
-#     created = models.DateTimeField(auto_now_add=True)  # first saved change
+class Meow(models.Model):
+    cat = models.ForeignKey(User, on_delete=models.CASCADE)
+    catRoom = models.ForeignKey(CatRoom, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)  # every saved change
+    created = models.DateTimeField(auto_now_add=True)  # first saved change
 
-#     def __str__(self):
-#         return str(self.body[0:50])
+    def __str__(self):
+        return str(self.body[0:50])
