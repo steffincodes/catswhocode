e5d94abbf301deebdf4e6980112e16f0467654da
Steffin <steffin.codes@gmail.com>
2022-06-09 06:01:46 +0530

    🏗️ WIP: Authentication - Register

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/',views.loginRegister, name = 'login'),
-    path('logout/',views.logoutUser, name = 'logout'),
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
 
     path('', views.home, name='home'),
     path('cat-room/<str:pk>', views.catRoom, name='catRoom'),
--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from .models import CatRoom, Topic
@@ -11,11 +12,12 @@ from .forms import CatRoomForm
 # Create your views here.
 
 
-def loginRegister(request):
+def loginUser(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -28,13 +30,29 @@ def loginRegister(request):
         else:
             messages.error(request, 'Authetication Failed')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerUser(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Unable to register...')
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
